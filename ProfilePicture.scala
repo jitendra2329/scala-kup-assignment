6b2604dde4db07e2887f318c5efcf457cb745669
Jitendra <jitendra.kumar@knoldus.com>
2023-03-09 17:37:09 +0530

    Profile picture assignment is added

---
+++ ProfilePicture.scala
@@ -0,0 +1,33 @@
+//import java.util.InputMismatchException
+import scala.io.StdIn
+
+object ProfilePicture extends  App {
+
+  try {
+    print("Enter the length of the square Frame : ")
+    val lengthOfFrame: Int = StdIn.readInt()
+
+    println("Enter the dimensions of the picture :  ")
+    print("width : ")
+    val width: Int = StdIn.readInt()
+    print("height : ")
+    val height: Int = StdIn.readInt()
+
+    println(uploadProfilePicture(lengthOfFrame, width, height))
+  } catch {
+      case e : NumberFormatException =>{
+        println("Please Enter numbers only !!")
+      }
+  }
+
+  def uploadProfilePicture(lengthOfFrame: Int, width: Int, height: Int) : String = {
+
+    if(width < lengthOfFrame || height < lengthOfFrame){
+       "Upload another picture !!"
+    } else if (height == width) {
+       "Upload successful !!"
+    } else {
+       "Crop the picture !!"
+    }
+  }
+}
